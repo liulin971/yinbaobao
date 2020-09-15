fb2076e625cbd3df2c0b3e76cbbce5630dfd409c
liulin971 <13452779920@163.com>
2020-09-15 23:17:20 +0800

    配置文件

---
+++ src/main/java/com/liulin/soft/yinbaobao/config/DrudConfig.java
@@ -0,0 +1,55 @@
+package com.liulin.soft.yinbaobao.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DrudConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return new DruidDataSource();
+    }
+
+
+    //配置druid的监控
+    //1.配置管理后台的servlet
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        Map<String,String> mpa = new HashMap<>();
+        mpa.put("loginUsername","admin");
+        mpa.put("loginPassword","123456");
+        mpa.put("allow","");//默认允许所有
+//        mpa.put("deny","");//
+        bean.setInitParameters(mpa);
+        return bean;
+    }
+
+    //2.配置一个监控的filter
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean bean =new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        Map<String ,String> map = new HashMap<>();
+        map.put("exclusions","*.js,*.css,/druid/*");
+        bean.setInitParameters(map);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+
+        return bean;
+
+    }
+}
--- src/test/java/com/liulin/soft/yinbaobao/YinbaobaoApplicationTests.java
+++ src/test/java/com/liulin/soft/yinbaobao/YinbaobaoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.liulin.soft.yinbaobao;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class YinbaobaoApplicationTests {
@@ -9,6 +14,20 @@ class YinbaobaoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("测试提交");
+    }
+
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void contextLoads1() throws SQLException {
+        System.out.println(dataSource.getClass());
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+
     }
 
 }
