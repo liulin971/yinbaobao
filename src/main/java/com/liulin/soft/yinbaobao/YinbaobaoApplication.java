3514a948a17b3229427dab238ed316f8bbf24935
liulin <13452779920@163.com>
2020-09-15 12:54:46 +0800

    第一次提交

---
+++ src/main/java/com/liulin/soft/yinbaobao/YinbaobaoApplication.java
@@ -0,0 +1,13 @@
+package com.liulin.soft.yinbaobao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinbaobaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YinbaobaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liulin/soft/yinbaobao/YinbaobaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liulin.soft.yinbaobao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinbaobaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
