d6d5f708141c314bc6250543e63a78c3c0e4060d
liulin <13452779920@163.com>
2020-09-25 11:24:22 +0800

    配置环境bjb，ts，druid启动等

--- src/main/java/com/liulin/soft/yinbaobao/config/DrudConfig.java
+++ src/main/java/com/liulin/soft/yinbaobao/config/DrudConfig.java
@@ -28,16 +28,23 @@ public class DrudConfig {
     Logger logger = LoggerFactory.getLogger(getClass());
 
 
-    //配置druid的监控
-    //1.配置管理后台的servlet
+
+
+    /**
+     *  配置druid的监控
+     *  1.配置管理后台的servlet
+     *  访问网址: http://127.0.0.1:8080/druid
+     * @return
+     */
     @Bean
     public ServletRegistrationBean servletRegistrationBean(){
-        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");////指定拦截器只拦截druid管理页面的请求
         Map<String,String> mpa = new HashMap<>();
         mpa.put("loginUsername","admin");
         mpa.put("loginPassword","123456");
-        mpa.put("allow","");//默认允许所有
+        mpa.put("allow", "");         //ip白名单，如果没有设置或为空，则表示允许所有访问
 //        mpa.put("deny","");//
+        mpa.put("resetEnable", "true");       //是否允许重置druid的统计信息
         bean.setInitParameters(mpa);
         return bean;
     }
@@ -49,7 +56,8 @@ public class DrudConfig {
         bean.setFilter(new WebStatFilter());
 
         Map<String ,String> map = new HashMap<>();
-        map.put("exclusions","*.js,*.css,/druid/*");
+        map.put("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        //bean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         bean.setInitParameters(map);
         bean.setUrlPatterns(Arrays.asList("/*"));
 
